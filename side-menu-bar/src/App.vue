0d72eb24b8619a043d5e8070a827ff850f348a48
huhaifeng <huhaifeng@wpdata.com>
2025-06-04 18:29:11 +0800

    side menu vue project

---
+++ side-menu-bar/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <index></index>
+  </div>
+</template>
+
+<script>
+import Index from './view/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Index
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background: url('assets/bg.png') no-repeat;
+  background-size: 100vw 100vh;
+}
+</style>
